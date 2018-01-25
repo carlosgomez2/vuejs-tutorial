542a86e49fdcfe31b5accb1ebc510bf425d7608e
Carlos Gomez <carlosgomez.deb@gmail.com>
2018-01-25 12:07:19 -0600

    added class 13 'punchbag game'

--- app.js
+++ app.js
@@ -119,3 +119,26 @@ let app7 = new Vue({
     ]
   }
 })
+
+let punchbagGame = new Vue({
+  el: '#punchbagGame',
+  data: {
+    health: 100,
+    ended: false
+  },
+  methods: {
+    punch: function(){
+      this.health -= 10
+      if(this.health <= 0){
+        this.ended = true
+      }
+    },
+    restart: function(){
+      this.health = 100
+      this.ended = false
+    }
+  },
+  computed: {
+
+  }
+})
